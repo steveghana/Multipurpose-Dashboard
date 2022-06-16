5bc0e0356ec12b4598f07e3538ee603c1a035876
steveghana <boatengstephen707@gmail.com>
2022-06-16 22:20:48 +0000

    fix customer checkbox funcitonality

--- src/pages/customers.tsx
+++ src/pages/customers.tsx
@@ -11,8 +11,31 @@ import GridModal from "../components/modal";
 import { useStateContext } from "../context/context";
 // interface
 const Customers: React.FC = () => {
+  const checkboxRef = React.useRef<HTMLDivElement>(null);
   const { currentPage, postperPage, setCurrentPage } = useStateContext();
+  const [isChecked, setisChecked] = React.useState(false);
+  const [customercheck, setcustomercheck] = React.useState({
+    check: false,
+    number: 0,
+  });
+  const [finalcheck, setFinalcheck] = React.useState(true);
   const indexOfLastCustomer = currentPage * postperPage;
+  const allChecked = () => {
+    if (checkboxRef.current === null) return;
+    const element = checkboxRef?.current?.querySelectorAll<any>("input");
+    element.forEach((el) => {
+      if (!isChecked) {
+        el.checked = true;
+        setisChecked(true);
+      } else {
+        setisChecked(false);
+      }
+    });
+  };
+  const customerChecked = (i: number) => {
+    setcustomercheck({ check: true, number: i });
+  };
+
   const indexOfFirstCustomer = indexOfLastCustomer - postperPage;
   const customers = customersData.slice(
     indexOfFirstCustomer,
@@ -28,25 +51,42 @@ const Customers: React.FC = () => {
         }}
       >
         <Header bigtext="Customers" smallText="Page" />
-        <div className="customer_list-box">
+        <div ref={checkboxRef} className="customer_list-box">
           <div className="customer_gridheaders">
-            {customersGrid.map(({ headerText, textAlign, width, type }) => (
-              <div
-                className="customer_header"
-                style={{ width, textAlign: "center" }}
-              >
-                {type === "checkbox" ? <Checkbox /> : headerText}
-              </div>
-            ))}
+            {customersGrid.map(
+              ({ headerText, textAlign, width, type }, index) => (
+                <div
+                  key={index}
+                  className="customer_header"
+                  style={{ width, textAlign: "center" }}
+                >
+                  {type === "checkbox" ? (
+                    <Checkbox onClick={allChecked} />
+                  ) : (
+                    headerText
+                  )}
+                </div>
+              )
+            )}
           </div>
           {customers.map((info, index) => {
             return (
-              <div className="customerGrid_container">
+              <div key={index} className="customerGrid_container">
                 <div
                   className="order_image"
                   style={{ width: customersGrid[0].width }}
                 >
-                  {<Checkbox />}
+                  {
+                    <Checkbox
+                      className="checkbox"
+                      onClick={({ target }) => customerChecked(index)}
+                      checked={
+                        isChecked === true && index === customercheck.number
+                          ? false
+                          : isChecked && true
+                      }
+                    />
+                  }
                 </div>
 
                 <CustomerGridImage
